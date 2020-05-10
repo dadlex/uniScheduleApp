f310875f8da7ded4e58e5416ba70d3143cc19bc3
Alex <alex15051997@yandex.ru>
2020-05-10 19:31:23 +0300

    add teacher unit test

---
+++ app/src/androidTest/java/com/simply/schedule/AddTeacherTest.kt
@@ -0,0 +1,387 @@
+package com.simply.schedule
+
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+import androidx.test.runner.AndroidJUnit4
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.`is`
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.TypeSafeMatcher
+import org.hamcrest.core.IsInstanceOf
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class AddTeacherTest {
+
+    @Rule
+    @JvmField
+    var mActivityTestRule = ActivityTestRule(MainActivity::class.java)
+
+    @Test
+    fun addTeacherTest() {
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        Thread.sleep(700)
+
+        val textInputEditText = onView(
+            allOf(
+                withId(R.id.input_username),
+                childAtPosition(
+                    childAtPosition(
+                        withClassName(`is`("com.google.android.material.textfield.TextInputLayout")),
+                        0
+                    ),
+                    0
+                )
+            )
+        )
+        textInputEditText.perform(scrollTo(), replaceText("alex"), closeSoftKeyboard())
+
+        val textInputEditText2 = onView(
+            allOf(
+                withId(R.id.input_password),
+                childAtPosition(
+                    childAtPosition(
+                        withClassName(`is`("com.google.android.material.textfield.TextInputLayout")),
+                        0
+                    ),
+                    0
+                )
+            )
+        )
+        textInputEditText2.perform(scrollTo(), replaceText("qwwe"), closeSoftKeyboard())
+
+        val textInputEditText3 = onView(
+            allOf(
+                withId(R.id.input_password), withText("qwwe"),
+                childAtPosition(
+                    childAtPosition(
+                        withClassName(`is`("com.google.android.material.textfield.TextInputLayout")),
+                        0
+                    ),
+                    0
+                )
+            )
+        )
+        textInputEditText3.perform(scrollTo(), click())
+
+        val textInputEditText4 = onView(
+            allOf(
+                withId(R.id.input_password), withText("qwwe"),
+                childAtPosition(
+                    childAtPosition(
+                        withClassName(`is`("com.google.android.material.textfield.TextInputLayout")),
+                        0
+                    ),
+                    0
+                )
+            )
+        )
+        textInputEditText4.perform(scrollTo(), replaceText("qwe"))
+
+        val textInputEditText5 = onView(
+            allOf(
+                withId(R.id.input_password), withText("qwe"),
+                childAtPosition(
+                    childAtPosition(
+                        withClassName(`is`("com.google.android.material.textfield.TextInputLayout")),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        textInputEditText5.perform(closeSoftKeyboard())
+
+        val textInputEditText6 = onView(
+            allOf(
+                withId(R.id.input_password), withText("qwe"),
+                childAtPosition(
+                    childAtPosition(
+                        withClassName(`is`("com.google.android.material.textfield.TextInputLayout")),
+                        0
+                    ),
+                    0
+                )
+            )
+        )
+        textInputEditText6.perform(scrollTo(), click())
+
+        val textInputEditText7 = onView(
+            allOf(
+                withId(R.id.input_password), withText("qwe"),
+                childAtPosition(
+                    childAtPosition(
+                        withClassName(`is`("com.google.android.material.textfield.TextInputLayout")),
+                        0
+                    ),
+                    0
+                )
+            )
+        )
+        textInputEditText7.perform(scrollTo(), replaceText("qwe1"))
+
+        val textInputEditText8 = onView(
+            allOf(
+                withId(R.id.input_password), withText("qwe1"),
+                childAtPosition(
+                    childAtPosition(
+                        withClassName(`is`("com.google.android.material.textfield.TextInputLayout")),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        textInputEditText8.perform(closeSoftKeyboard())
+
+        val materialButton = onView(
+            allOf(
+                withId(R.id.btn_login), withText("Login"),
+                childAtPosition(
+                    childAtPosition(
+                        withClassName(`is`("android.widget.ScrollView")),
+                        0
+                    ),
+                    3
+                )
+            )
+        )
+        materialButton.perform(scrollTo(), click())
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        Thread.sleep(700)
+
+        val floatingActionButton = onView(
+            allOf(
+                withId(R.id.fab),
+                childAtPosition(
+                    allOf(
+                        withId(R.id.container),
+                        childAtPosition(
+                            withId(android.R.id.content),
+                            0
+                        )
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        floatingActionButton.perform(click())
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        Thread.sleep(700)
+
+        val cardView = onView(
+            childAtPosition(
+                childAtPosition(
+                    withClassName(`is`("android.widget.ScrollView")),
+                    0
+                ),
+                5
+            )
+        )
+        cardView.perform(scrollTo(), click())
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        Thread.sleep(700)
+
+        val floatingActionButton2 = onView(
+            allOf(
+                withId(R.id.fab),
+                childAtPosition(
+                    childAtPosition(
+                        withId(android.R.id.content),
+                        0
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        floatingActionButton2.perform(click())
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        Thread.sleep(700)
+
+        val appCompatEditText = onView(
+            allOf(
+                withId(R.id.etName),
+                childAtPosition(
+                    allOf(
+                        withId(R.id.vsNameSwitcher),
+                        childAtPosition(
+                            withClassName(`is`("android.widget.LinearLayout")),
+                            1
+                        )
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatEditText.perform(click())
+
+        val appCompatEditText2 = onView(
+            allOf(
+                withId(R.id.etName),
+                childAtPosition(
+                    allOf(
+                        withId(R.id.vsNameSwitcher),
+                        childAtPosition(
+                            withClassName(`is`("android.widget.LinearLayout")),
+                            1
+                        )
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatEditText2.perform(click())
+
+        val appCompatEditText3 = onView(
+            allOf(
+                withId(R.id.etName),
+                childAtPosition(
+                    allOf(
+                        withId(R.id.vsNameSwitcher),
+                        childAtPosition(
+                            withClassName(`is`("android.widget.LinearLayout")),
+                            1
+                        )
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatEditText3.perform(replaceText("Na"), closeSoftKeyboard())
+
+        val appCompatEditText4 = onView(
+            allOf(
+                withId(R.id.etPhone),
+                childAtPosition(
+                    allOf(
+                        withId(R.id.vsPhoneSwitcher),
+                        childAtPosition(
+                            withClassName(`is`("android.widget.LinearLayout")),
+                            1
+                        )
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatEditText4.perform(replaceText("1234"), closeSoftKeyboard())
+
+        val appCompatEditText5 = onView(
+            allOf(
+                withId(R.id.etEmail),
+                childAtPosition(
+                    allOf(
+                        withId(R.id.vsEmailSwitcher),
+                        childAtPosition(
+                            withClassName(`is`("android.widget.LinearLayout")),
+                            1
+                        )
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatEditText5.perform(replaceText("email"), closeSoftKeyboard())
+
+        val floatingActionButton3 = onView(
+            allOf(
+                withId(R.id.fab),
+                childAtPosition(
+                    childAtPosition(
+                        withId(android.R.id.content),
+                        0
+                    ),
+                    2
+                ),
+                isDisplayed()
+            )
+        )
+        floatingActionButton3.perform(click())
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        Thread.sleep(700)
+
+        val viewGroup = onView(
+            allOf(
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.rvTeachers),
+                        1
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        viewGroup.check(matches(isDisplayed()))
+
+        val textView = onView(
+            allOf(
+                withId(R.id.tvName), withText("Na"),
+                childAtPosition(
+                    childAtPosition(
+                        IsInstanceOf.instanceOf(android.widget.FrameLayout::class.java),
+                        0
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        textView.check(matches(withText("Na")))
+    }
+
+    private fun childAtPosition(
+        parentMatcher: Matcher<View>, position: Int
+    ): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
+    }
+}
