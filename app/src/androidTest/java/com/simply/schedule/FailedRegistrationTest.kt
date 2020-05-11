b5ac026b1e13faf7edf1d77a2b1847bca429be92
Alex <alex15051997@yandex.ru>
2020-05-11 17:26:34 +0300

    fix failed reg test

--- app/src/androidTest/java/com/simply/schedule/FailedRegistrationTest.kt
+++ app/src/androidTest/java/com/simply/schedule/FailedRegistrationTest.kt
@@ -1,37 +1,26 @@
 package com.simply.schedule
 
 
-import androidx.test.espresso.DataInteraction
-import androidx.test.espresso.ViewInteraction
+import android.view.View
+import android.view.ViewGroup
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.filters.LargeTest
 import androidx.test.rule.ActivityTestRule
 import androidx.test.runner.AndroidJUnit4
-import android.view.View
-import android.view.ViewGroup
-import android.view.ViewParent
-
-import androidx.test.InstrumentationRegistry.getInstrumentation
-import androidx.test.espresso.Espresso.onData
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.Espresso.pressBack
-import androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu
-import androidx.test.espresso.action.ViewActions.*
-import androidx.test.espresso.assertion.ViewAssertions.*
-import androidx.test.espresso.matcher.ViewMatchers.*
-
-import com.simply.schedule.R
-
 import org.hamcrest.Description
 import org.hamcrest.Matcher
+import org.hamcrest.Matchers.`is`
+import org.hamcrest.Matchers.allOf
 import org.hamcrest.TypeSafeMatcher
 import org.hamcrest.core.IsInstanceOf
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-import org.hamcrest.Matchers.allOf
-import org.hamcrest.Matchers.anything
-import org.hamcrest.Matchers.`is`
 /*
 1. Go to create account page
 2. Fill in incorrect data. A password that is shorter than 4 symbols
@@ -39,6 +28,7 @@ import org.hamcrest.Matchers.`is`
 4. Check that reenter text view has error text "passwords do not match"
 5. Check that page is still the same and there is no popup window
  */
+
 @LargeTest
 @RunWith(AndroidJUnit4::class)
 class FailedRegistrationTest {
@@ -49,93 +39,152 @@ class FailedRegistrationTest {
 
     @Test
     fun failedRegistrationTest() {
-         // Added a sleep statement to match the app's execution delay.
- // The recommended way to handle such scenarios is to use Espresso idling resources:
-  // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
-Thread.sleep(700)
-        
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        Thread.sleep(700)
+
         val appCompatTextView = onView(
-allOf(withId(R.id.link_signup), withText("No account yet? Create one"),
-childAtPosition(
-childAtPosition(
-withClassName(`is`("android.widget.ScrollView")),
-0),
-4)))
+            allOf(
+                withId(R.id.link_signup), withText("No account yet? Create one"),
+                childAtPosition(
+                    childAtPosition(
+                        withClassName(`is`("android.widget.ScrollView")),
+                        0
+                    ),
+                    4
+                )
+            )
+        )
         appCompatTextView.perform(scrollTo(), click())
-        
-         // Added a sleep statement to match the app's execution delay.
- // The recommended way to handle such scenarios is to use Espresso idling resources:
-  // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
-Thread.sleep(700)
-        
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        Thread.sleep(700)
+
         val textInputEditText = onView(
-allOf(withId(R.id.input_username),
-childAtPosition(
-childAtPosition(
-withClassName(`is`("com.google.android.material.textfield.TextInputLayout")),
-0),
-0)))
+            allOf(
+                withId(R.id.input_username),
+                childAtPosition(
+                    childAtPosition(
+                        withClassName(`is`("com.google.android.material.textfield.TextInputLayout")),
+                        0
+                    ),
+                    0
+                )
+            )
+        )
         textInputEditText.perform(scrollTo(), replaceText("fjhq"), closeSoftKeyboard())
-        
+
         val textInputEditText2 = onView(
-allOf(withId(R.id.input_password),
-childAtPosition(
-childAtPosition(
-withClassName(`is`("com.google.android.material.textfield.TextInputLayout")),
-0),
-0)))
+            allOf(
+                withId(R.id.input_password),
+                childAtPosition(
+                    childAtPosition(
+                        withClassName(`is`("com.google.android.material.textfield.TextInputLayout")),
+                        0
+                    ),
+                    0
+                )
+            )
+        )
         textInputEditText2.perform(scrollTo(), replaceText("1"), closeSoftKeyboard())
-        
+
         val textInputEditText3 = onView(
-allOf(withId(R.id.input_reEnterPassword),
-childAtPosition(
-childAtPosition(
-withClassName(`is`("com.google.android.material.textfield.TextInputLayout")),
-0),
-0)))
-        textInputEditText3.perform(scrollTo(), replaceText("1"), closeSoftKeyboard())
-        
+            allOf(
+                withId(R.id.input_reEnterPassword),
+                childAtPosition(
+                    childAtPosition(
+                        withClassName(`is`("com.google.android.material.textfield.TextInputLayout")),
+                        0
+                    ),
+                    0
+                )
+            )
+        )
+        textInputEditText3.perform(scrollTo(), replaceText("2"), closeSoftKeyboard())
+
         val materialButton = onView(
-allOf(withId(R.id.btn_signup), withText("Create Account"),
-childAtPosition(
-childAtPosition(
-withClassName(`is`("android.widget.ScrollView")),
-0),
-4)))
+            allOf(
+                withId(R.id.btn_signup), withText("Create Account"),
+                childAtPosition(
+                    childAtPosition(
+                        withClassName(`is`("android.widget.ScrollView")),
+                        0
+                    ),
+                    4
+                )
+            )
+        )
         materialButton.perform(scrollTo(), click())
-        
+
         val linearLayout = onView(
-allOf(childAtPosition(
-childAtPosition(
-IsInstanceOf.instanceOf(android.widget.LinearLayout::class.java),
-3),
-1),
-isDisplayed()))
+            allOf(
+                childAtPosition(
+                    childAtPosition(
+                        IsInstanceOf.instanceOf(android.widget.LinearLayout::class.java),
+                        3
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
         linearLayout.check(matches(isDisplayed()))
-        
+
         val button = onView(
-allOf(withId(R.id.btn_signup),
-childAtPosition(
-childAtPosition(
-IsInstanceOf.instanceOf(android.widget.ScrollView::class.java),
-0),
-4),
-isDisplayed()))
+            allOf(
+                withId(R.id.btn_signup),
+                childAtPosition(
+                    childAtPosition(
+                        IsInstanceOf.instanceOf(android.widget.ScrollView::class.java),
+                        0
+                    ),
+                    4
+                ),
+                isDisplayed()
+            )
+        )
         button.check(matches(isDisplayed()))
-        
+
         val editText = onView(
-allOf(withId(R.id.input_reEnterPassword), withText("1"),
-childAtPosition(
-childAtPosition(
-IsInstanceOf.instanceOf(android.widget.LinearLayout::class.java),
-0),
-0),
-isDisplayed()))
-        editText.check(matches(withText("1")))
-        }
-    
+            allOf(
+                withId(R.id.input_reEnterPassword), withText("2"),
+                childAtPosition(
+                    childAtPosition(
+                        IsInstanceOf.instanceOf(android.widget.LinearLayout::class.java),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+
+        editText.check(matches(withText("2")))
+
+        editText.check(matches(hasErrorText("Passwords do not match")));
+
+        val textViewReg = onView(
+            allOf(
+                withId(android.R.id.message), withText("Registration successful"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.scrollView),
+                        0
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        textViewReg.check(doesNotExist())
+    }
+
     private fun childAtPosition(
-            parentMatcher: Matcher<View>, position: Int): Matcher<View> {
+        parentMatcher: Matcher<View>, position: Int
+    ): Matcher<View> {
 
         return object : TypeSafeMatcher<View>() {
             override fun describeTo(description: Description) {
@@ -150,4 +199,4 @@ isDisplayed()))
             }
         }
     }
-    }
+}
