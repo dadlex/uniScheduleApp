ec5e70cdd53cb5ace4e7eadd0e63995adb5b516f
Denis Pakhorukov <denis.pakhorukov@gmail.com>
2020-05-15 23:02:01 +0300

    Add logout functionality

--- app/src/main/java/com/simply/schedule/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/simply/schedule/ui/settings/SettingsFragment.kt
@@ -1,31 +1,50 @@
 package com.simply.schedule.ui.settings
 
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Button
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import com.simply.schedule.R
+import com.simply.schedule.ui.login.LoginActivity
 
 class SettingsFragment : Fragment() {
 
-    private lateinit var settingsViewModel: SettingsViewModel
+    private lateinit var mLogoutButton: Button
+    private lateinit var mSharedPreferences: SharedPreferences
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        settingsViewModel =
-            ViewModelProviders.of(this).get(SettingsViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_settings, container, false)
-//        val textView: TextView = root.findViewById(R.id.text_settings)
-//        settingsViewModel.text.observe(this, Observer {
-//            textView.text = it
-//        })
+        mSharedPreferences = context!!.getSharedPreferences("Schedule", Context.MODE_PRIVATE)
+
+        mLogoutButton = root.findViewById<Button>(R.id.logout_button)
+        mLogoutButton.setOnClickListener {
+            AlertDialog.Builder(root.context)
+                .setMessage("Do you want to logout?")
+                .setPositiveButton(android.R.string.yes) { dialog, _ ->
+                    logout()
+                }
+                .setNegativeButton(android.R.string.no) { dialog, _ ->
+                    dialog.cancel()
+                }
+                .create().show()
+        }
         return root
+    }
+
+    private fun logout() {
+        mSharedPreferences.edit().remove("credentials").apply()
+        val intent = Intent(context, LoginActivity::class.java)
+        startActivity(intent)
+        activity!!.finish()
     }
 }
